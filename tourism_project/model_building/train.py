7d946906970742918ef4231200dfde3353b180f3
fadeyemi-a11y <fadeyemi@gmail.com>
2026-09-19 11:42:29 +0100

    update tourism_project/model_building/train.py

--- tourism_project/model_building/train.py
+++ tourism_project/model_building/train.py
@@ -108,7 +108,7 @@ with mlflow.start_run():
 
     # Save the model next to app.py so the Streamlit app can load it directly,
     # and log it as an MLflow artifact for traceability
-    model_path = "tourism_project/model_deployment/best_tourism_package_model.joblib"   
+    model_path = "tourism_project/deployment/best_tourism_package_model.joblib"   
     # Specify the local file path (inside tourism_project/deployment/) where the trained model should be saved.
     joblib.dump(best_model, model_path)  
     # complete the code to save the model
